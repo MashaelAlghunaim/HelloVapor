ecf2adde4b45ac44c37bafeb852296f6550a048e
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2023-03-15 10:06:25 +0300

    .

---
+++ Sources/App/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mashael Alghunaim on 23/08/1444 AH.
+//
+
+import Foundation
+import Vapor
+
+
+struct Animals: Content{
+    let petName: String
+    let species: String
+    let ownerName: String
+    let contactNumber: Int
+    
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,15 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+//    app.get { req async in
+//        "It works!"
+//    }
+
+//    app.get("hello") { req async -> String in
+//        "Hello, world!"
+    app.get("Animals"){ request in
+        let animal: [Animals] = [Animals(petName: "Dog", species: "Goldenretreiver", ownerName:  "Mashael", contactNumber: 064958483)]
+        return animal
+    }
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-}
